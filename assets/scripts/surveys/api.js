a690ae95e08ba55aa69778c585642c35f3ca645f
soynog <nicolas.o.garcia@gmail.com>
2016-05-13 16:53:45 -0400

    Comment Outline for Survey API Script

---
+++ assets/scripts/surveys/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Require app-data to get user token, etc.
+
+// Functions for making API calls. Each function should make an AJAX call and take a success and failure callback.
+
+// getUserSurveys function - GET
+// shows all surveys associated with a given user
+
+// showSurvey function - GET
+// get the info for a single survey based on its id -- for answering a survey
+// is this necessary? Or will it happen automatically when someone enters the url into the browser?
+
+// Sign Out Function - DELETE
+
+// Change PW Function - PATCH
+
+// Export functions as module
