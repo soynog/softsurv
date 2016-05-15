a5bcaf0fbe9b67e02329e5724ef6f91fc97b027c
soynog <nicolas.o.garcia@gmail.com>
2016-05-15 10:10:45 -0400

    Add User Auth API Methods
    - Use script from resume-tailor project as template

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -18,9 +18,54 @@ const signUp = (success, failure, data) => {
 };
 
 // Sign In Function - POST
+const signIn = (success, failure, data) => {
+  console.log("Requesting Sign-in");
+  console.log(data);
+  $.ajax({
+    method: 'POST',
+    url: app.api + '/sign-in',
+    data,
+  }).done(success)
+  .fail(failure);
+};
 
 // Sign Out Function - DELETE
+const signOut = (success, failure) => {
+  console.log("Requesting Sign-out");
+  console.log(app);
+  if(app.user) {
+    $.ajax({
+      method: 'DELETE',
+      url: app.api + '/sign-out/' + app.user.id,
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      }
+    }).done(success)
+    .fail(failure);
+  }
+};
 
 // Change PW Function - PATCH
+const changePW = (success, failure, data) => {
+  console.log("Requesting Password Change");
+  console.log(data);
+  if(app.user) {
+    $.ajax({
+      method: 'PATCH',
+      url: app.api + '/change-password/' + app.user.id,
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      },
+      data,
+    }).done(success)
+    .fail(failure);
+  }
+};
 
 // Export functions as module
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePW,
+};
