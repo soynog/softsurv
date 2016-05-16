fe54b6a8c7f3b5d042c58e4b3037667332d24f24
soynog <nicolas.o.garcia@gmail.com>
2016-05-16 09:26:35 -0400

    Prepare to show survey response form
    - Create empty handlebars template for form
    - Use getUrls function to check if survey url has been submitted

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,18 @@
 const authEvents = require('./auth/events');
 const display = require('./display');
 const surveyEvents = require('./surveys/events');
+const urlParams = require('./url-params');
 
 $(() => {
-  authEvents.addHandlers();
-  display.renderNewSurveyForm();
-  display.showAllUserSurveys();
-  surveyEvents.addHandlers();
+  let params = urlParams.getUrlParams();
+  if (params) {
+    console.log("Grabbing params");
+    console.log(params);
+  } else {
+    console.log("Ain't no paramters heah");
+    authEvents.addHandlers();
+    display.renderNewSurveyForm();
+    display.showAllUserSurveys();
+    surveyEvents.addHandlers();
+  }
 });
